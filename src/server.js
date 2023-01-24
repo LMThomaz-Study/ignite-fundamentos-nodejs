25b6376bb2951edd01376a3532ffdaa6f2082565
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-01-24 12:26:55 -0300

    Save users in stateful

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((request, response) => {
   const {method, url} = request
   
   if (method === 'GET' && url === '/users') {
-    return response.end('Listagem de usuário')
+    return response.setHeader('Content-Type', 'application/json').end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      name: 'John Doe',
+      email: 'john.doe@example.com'
+    })
+
     return response.end('Criação de usuário')
   }
 
