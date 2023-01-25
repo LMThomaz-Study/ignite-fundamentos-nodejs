d9e5591b7ebbc2c6cce8adf7b7a36ddbdc2e9fd7
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-01-24 21:52:41 -0300

    request body in JSON (Stream and Buffer)

--- src/server.js
+++ src/server.js
@@ -2,19 +2,34 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((request, response) => {
-  const {method, url} = request
-  
+const server = http.createServer(async (request, response) => {
+  const { method, url } = request
+
+  const buffers = []
+
+  for await (const chunk of request) {
+    buffers.push(chunk)
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    request.body = null
+  }
+
   if (method === 'GET' && url === '/users') {
-    return response.setHeader('Content-Type', 'application/json').end(JSON.stringify(users))
+    return response
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-
+    const { name, email } = request.body
 
     users.push({
-      name: 'John Doe',
-      email: 'john.doe@example.com'
+      id: users.length + 1,
+      name,
+      email,
     })
 
     return response.writeHead(201).end()
